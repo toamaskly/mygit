89db4af89a869664e18d684f9d44d0816b647220
ty <529009325@qq.com>
2024-04-21 16:23:40 +0800

    ok,i agree!

--- app/src/main/java/com/example/myfragmentx/MainActivity.java
+++ app/src/main/java/com/example/myfragmentx/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("自己","我做出了第一次修改");
     }
 
     public void jumpFragment(View view) {
